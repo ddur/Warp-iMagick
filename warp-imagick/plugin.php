5e9aec99b2fbc7ed3f3b1eebece86edb1e172f74
ddur <3501612+ddur@users.noreply.github.com>
2022-04-25 00:00:19 +0200

    Source version 1.9.7

---
+++ warp-imagick/plugin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © 2017-2022 Dragan Đurić. All rights reserved.
+ *
+ * @package warp-imagick
+ * @license GNU General Public License Version 2.
+ * @copyright © 2017-2022. All rights reserved.
+ * @author Dragan Đurić
+ * @link https://warp-imagick.pagespeed.club/
+ *
+ * This copyright notice, source files, licenses and other included
+ * materials are protected by U.S. and international copyright laws.
+ * You are not allowed to remove or modify this or any other
+ * copyright notice contained within this software package.
+ */
+
+namespace ddur\Warp_iMagick;
+
+use \ddur\Warp_iMagick\Plugin;
+
+defined( 'ABSPATH' ) || die( -1 );
+
+require_once __DIR__ . '/classes/autoloader.php';
+
+Plugin::once( __DIR__ . '/index.php' );
